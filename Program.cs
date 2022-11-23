b0e4706eaf7e61d416d89d25a0c5e5e9fe781337
Felippe-Rozendo <felippem001@gmail.com>
2022-11-23 15:34:16 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using ClosedXML.Excel;
+
+var xls = new XLWorkbook(@"C:\Nova pasta\testeReader.xlsx"); // coloque o caminho do arquivo a ser lido
+var planilha = xls.Worksheets.First();
+var totalLinhas = planilha.Rows().Count();
+var totalCol = planilha.Columns().Count();
+
+for (int l = 1; l < totalLinhas; l++)
+{
+    for (int t = 1; t < totalCol; t++)
+    {
+        var linha = planilha.Row(l).Cell(t).Value.ToString();
+        Console.Write(linha + " ");
+    }
+    Console.WriteLine();
+}
